b287df3fa057338c325ab1388a388d90e439a0d6
junshen <redwolf85911@gmail.com>
2016-08-04 00:13:14 +0000

    add install vimplug for regular vim step

--- brew.sh
+++ brew.sh
@@ -208,5 +208,10 @@ chsh -s /usr/local/bin/zsh
 
 
 # vim - plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
